3ba42e098e825a4774d15719f4c75bd92970e115
Lian Paul <lianpaulmolo08@gmail.com>
2021-09-04 12:15:37 +0800

    fixing indent spacing

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const accordion = () => {
-    const questions = document.querySelectorAll('.question');
+	const questions = document.querySelectorAll('.question');
 
-    // looping for all questions
-    questions.forEach((question) => {
-        const btn = question.querySelector('.question-header');
-        
-        btn.addEventListener('click', () => {
+	// looping for all questions
+	questions.forEach((question) => {
+		const btn = question.querySelector('.question-header');
+		
+		btn.addEventListener('click', () => {
 
-            questions.forEach((item) => {
-              /*looping again all questions inside a click event then if the item which the params, is not equal to question outside then remove the show answer class */
-                if (item !== question) {
-                    item.classList.remove('show-answer');
-                }
-            })
+			questions.forEach((item) => {
+				/*looping again all questions inside a click event then if the item which the params, is not equal to question outside then remove the show answer class */
+				if (item !== question) {
+					item.classList.remove('show-answer');
+				}
+			})
 
-            question.classList.toggle('show-answer');
-        })
-    })
+			question.classList.toggle('show-answer');
+		})
+	})
 }
 
 accordion()
